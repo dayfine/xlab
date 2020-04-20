b561ae0dd00485c5fa08412f1eefd7ad853274b1
dayfine <26423504+dayfine@users.noreply.github.com>
2020-04-19 20:32:06 -0700

    a bit cleanup and setup textproto store structure

--- xlab/data/store/in_memory/impl.py
+++ xlab/data/store/in_memory/impl.py
@@ -11,24 +11,24 @@ from xlab.util.status import errors
 class InMemoryDataStore(DataStore):
     def __init__(self):
         # In memory data store has empty initial state
-        self.data: Dict[Tuple, List[DataEntry]] = defaultdict(list)
+        self._data: Dict[Tuple, List[DataEntry]] = defaultdict(list)
 
     def add(self, data_entry: DataEntry):
         key = make_key(data_entry)[:3]  # Do not use timestamp in key
-        data_entries = self.data[key]
+        data_entries = self._data[key]
         if data_entry.timestamp.ToDatetime() in {
                 e.timestamp.ToDatetime()
                 for e in data_entries
         }:
             raise errors.AlreadyExistsError(
                 f'The data entry to add already exists: {data_entry}')
-        self.data[key].append(data_entry)
-        self.data[key].sort(key=lambda e: e.timestamp.ToDatetime())
+        self._data[key].append(data_entry)
+        self._data[key].sort(key=lambda e: e.timestamp.ToDatetime())
 
     def read(self, lookup_key: LookupKey) -> DataEntry:
         expected_key = (lookup_key.data_space, lookup_key.symbol,
                         lookup_key.data_type)
-        data_entries = self.data[expected_key]
+        data_entries = self._data[expected_key]
         try:
             return next(e for e in data_entries
                         if e.timestamp.ToDatetime() == lookup_key.timestamp)
@@ -38,7 +38,7 @@ class InMemoryDataStore(DataStore):
 
     def lookup(self, lookup_key: LookupKey) -> List[DataEntry]:
         results = []
-        for key, data_entries in self.data.items():
+        for key, data_entries in self._data.items():
             if not self._key_matches(key, lookup_key): continue
             results.extend(
                 (e for e in data_entries if lookup_key.timestamp is None
@@ -46,7 +46,7 @@ class InMemoryDataStore(DataStore):
         return results
 
     def each(self, fn: Callable[[DataEntry], None]):
-        for key, data_entries in self.data.items():
+        for key, data_entries in self._data.items():
             for data_entry in data_entries:
                 fn(data_entry)
 
--- xlab/data/store/textproto/textproto.py
+++ xlab/data/store/textproto/textproto.py
@@ -1,27 +1,36 @@
-from typing import Dict, List
+from typing import Callable, Dict, List
 
+from xlab.data.proto.data_entry_pb2 import DataEntry, DataEntries
 from xlab.data.store.interface import DataStore, LookupKey
+from xlab.data.store.in_memory.impl import InMemoryDataStore
 from xlab.data.store.key import make_key
 from xlab.util.status import errors
 
-from xlab.data.proto.data_entry_pb2 import DataEntry, DataEntries
-
 
 class TextProtoDataStore(DataStore):
-    def __init__(self):
-        self.data: Dict[str, DataEntries] = {}
+    def __init__(self, data_store_path=""):
+        self._mem_store = InMemoryDataStore()
+        self._data_store_path = data_store_path
+        self.last_updated_key = None
+        self._load()
+
+    def _load(self):
+        # Load from directory of files.
+        pass
+
+    def _commit(self):
+        # update
+        pass
 
     def add(self, data_entry: DataEntry):
-        pass
+        self._mem_store.add(data_entry)
+        self._commit()
 
     def read(self, key: LookupKey) -> DataEntry:
-        pass
+        return self._mem_store.read(key)
 
     def lookup(self, key: LookupKey) -> List[DataEntry]:
-        pass
+        return self._mem_store.lookup(key)
 
     def each(self, fn: Callable[[DataEntry], None]):
-        pass
-
-    def commit(self):
-        pass
+        return self._mem_store.each(fn)
