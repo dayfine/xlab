be676731d2f8b9180002e369c3883c15fd75048d
dayfine <26423504+dayfine@users.noreply.github.com>
2020-04-25 22:53:08 -0700

    update unittest for iex data provider

--- xlab/data/provider/iex/provider.py
+++ xlab/data/provider/iex/provider.py
@@ -1,4 +1,4 @@
-import datetime as dt
+from datetime import date, datetime
 
 from typing import Dict, List
 
@@ -8,6 +8,7 @@ from xlab.data.provider.iex import api
 
 
 class IexDataProvider(interface.DataProvider):
+
     def __init__(self, iex_client: api.IexApiHttpClient):
         self._iex_client = iex_client
 
@@ -15,20 +16,22 @@ class IexDataProvider(interface.DataProvider):
     def get_data(
         self,
         symbol: str,
-        start_date: dt.date = dt.date.today(),
-        end_date: dt.date = dt.date.today()
+        start_date: date = date.today(),
+        end_date: date = date.today()
     ) -> Dict[str, List[data_entry_pb2.DataEntry]]:
         # TODO: handle dates properly.
-        if end_date != dt.date.today():
+        if end_date != date.today():
             pass
         api_response = self._iex_client.get_batch_quotes(symbol)
-        chart_series = api_response.json()[symbol]['chart']
+        data = api_response.json()
+        print('DATA: ', data)
+        chart_series = data[symbol]['chart']
 
-        now = dt.datetime.now()
+        now = datetime.now()
         close_results = []
         volume_results = []
         for chart_data in chart_series:
-            data_date = dt.datetime.strptime(chart_data['date'], '%Y-%m-%d')
+            data_date = datetime.strptime(chart_data['date'], '%Y-%m-%d')
 
             close_data = self._make_data_base(symbol, data_date, now)
             close_data.data_type = 'close'
@@ -42,11 +45,12 @@ class IexDataProvider(interface.DataProvider):
 
         return {'close': close_results, 'volume': volume_results}
 
-    def _make_data_base(self, symbol: str, data_date: dt.datetime,
-                        now: dt.datetime) -> data_entry_pb2.DataEntry:
+    def _make_data_base(self, symbol: str, data_date: datetime,
+                        now: datetime) -> data_entry_pb2.DataEntry:
         data_entry = data_entry_pb2.DataEntry()
         data_entry.symbol = symbol
         data_entry.data_space = data_entry_pb2.DataEntry.STOCK_DATA
+        print('dates: ', data_date, now)
         data_entry.timestamp.FromDatetime(data_date)
         data_entry.updated_at.FromDatetime(now)
         return data_entry
--- xlab/data/provider/iex/provider_test.py
+++ xlab/data/provider/iex/provider_test.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 import json
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from absl.testing import absltest
 import requests
 
 from xlab.data.provider.iex import api, provider
+from xlab.net.proto.testing import compare
 
 
 def _make_response(conent: str, status_code: int) -> requests.Response:
@@ -15,10 +17,12 @@ def _make_response(conent: str, status_code: int) -> requests.Response:
 
 
 class IexDataProviderTest(absltest.TestCase):
+
     def setUp(self):
         self.provider = provider.IexDataProvider(
             api.IexApiHttpClient('fake_token'))
 
+    @patch.object(provider, 'datetime', Mock(wraps=datetime))
     def test_get_quotes_success(self):
         expected_content = """
           {
@@ -34,18 +38,47 @@ class IexDataProviderTest(absltest.TestCase):
         with patch('requests.Session.get',
                    return_value=_make_response(expected_content,
                                                200)) as mock_get:
+            provider.datetime.now.return_value = datetime(2020, 12, 31)
             results = self.provider.get_data('SPY')
 
             _, kwargs = mock_get.call_args
             self.assertEqual(kwargs['params']['symbols'], 'SPY')
 
             self.assertEqual(len(results['close']), 2)
-            self.assertEqual(results['close'][0], None)
-            self.assertEqual(results['close'][1], None)
+            compare.assertProtoEqual(
+                self, results['close'][0], """
+                symbol: "SPY"
+                data_space: STOCK_DATA
+                data_type: "close"
+                value: 319.69
+                timestamp { seconds: 1583107200 }
+                updated_at { seconds: 1609372800 }""")
+            compare.assertProtoEqual(
+                self, results['close'][1], """
+                symbol: "SPY"
+                data_space: STOCK_DATA
+                data_type: "close"
+                value: 308.48
+                timestamp { seconds: 1583193600 }
+                updated_at { seconds: 1609372800 }""")
 
             self.assertEqual(len(results['close']), 2)
-            self.assertEqual(results['volume'][0], None)
-            self.assertEqual(results['volume'][1], None)
+            compare.assertProtoEqual(
+                self, results['volume'][0], """
+                symbol: "SPY"
+                data_space: STOCK_DATA
+                data_type: "volume"
+                value: 242964067.0
+                timestamp { seconds: 1583107200 }
+                updated_at { seconds: 1609372800 }""")
+            compare.assertProtoEqual(
+                self, results['volume'][1], """
+                symbol: "SPY"
+                data_space: STOCK_DATA
+                data_type: "volume"
+                value: 300862938.0
+                timestamp { seconds: 1583193600 }
+                updated_at { seconds: 1609372800 }""")
 
     def test_get_quotes_default_single_day(self):
         pass
