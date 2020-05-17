173998a711284993c34890f94a75d4d769a7a62f
dayfine <26423504+dayfine@users.noreply.github.com>
2020-05-16 22:24:25 -0700

    port absl::Time to python

--- xlab/base/time.py
+++ xlab/base/time.py
@@ -1,5 +1,118 @@
-from typing import NewType
+import datetime
+import time
 
-# A practical precision for saving data points.
-# This also helps ignore loss of precision in various cases.
-Seconds = NewType('Seconds', int)
+
+# Time class follows the interface of https://github.com/abseil/abseil-cpp/blob/master/absl/time/time.h.
+class Time(int):
+
+    def __new__(cls, ns=0):
+        return super(Time, cls).__new__(cls, ns)
+
+    def __add__(self, other):
+        return self.__class__(super(Time, self).__add__(other))
+
+    def __sub__(self, other):
+        return self.__class__(super(Time, self).__sub__(other))
+
+    def __mul__(self, other):
+        return self.__class__(super(Time, self).__mul__(other))
+
+    def __truediv__(self, other):
+        return self.__class__(super(Time, self).__truediv__(other))
+
+    def __str__(self):
+        seconds, nanoseconds = divmod(self, 1e9)
+        return f"Seconds: {seconds}, Nanoseconds: {nanoseconds}"
+
+    def __repr__(self):
+        return f"Time({self})"
+
+
+Duration = Time
+
+
+def Now() -> Time:
+    return Time(time.time_ns())
+
+
+def UnixEpoch() -> Time:
+    return Time()
+
+
+# Return the RFC3339 Full representation of the time.
+def FormatTime(t: Time, tz: datetime.timezone = None) -> str:
+    if tz is None:
+        tz = datetime.timezone.utc
+    rfc3339_secs = time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(int(t) / 1e9))
+    tz_offset = datetime.time(tzinfo=tz).strftime('%z')
+    # -0700 -> -07:00
+    tz_offset = ''.join((tz_offset[:3], ':', tz_offset[3:]))
+    return ''.join((rfc3339_secs, tz_offset))
+
+
+def ZeroDuration() -> Duration:
+    return Duration(0)
+
+
+def Nanoseconds(n: int) -> Duration:
+    return Duration(n)
+
+
+def Microseconds(n: int) -> Duration:
+    return Duration(n * 1e3)
+
+
+def Milliseconds(n: int) -> Duration:
+    return Duration(n * 1e6)
+
+
+def Seconds(n: int) -> Duration:
+    return Duration(n * 1e9)
+
+
+def Minutes(n: int) -> Duration:
+    return Duration(n * 1e9 * 60)
+
+
+def Hours(n: int) -> Duration:
+    return Duration(n * 1e9 * 60 * 60)
+
+
+def FromUnixNanos(nanos: int) -> Time:
+    return Time(nanos)
+
+
+def FromUnixMillis(millis: int) -> Time:
+    return Time(millis * 1e3)
+
+
+def FromUnixMicros(micros: int) -> Time:
+    return Time(micros * 1e6)
+
+
+def FromUnixSeconds(seconds: int) -> Time:
+    return Time(seconds * 1e9)
+
+
+def FromDatetime(dt: datetime.datetime) -> Time:
+    return Time(time.mktime(dt.timetuple()) * 1e9)
+
+
+def ToUnixNanos(t: Time) -> int:
+    return int(t)
+
+
+def ToUnixMillis(t: Time) -> int:
+    return int(t) / 1e3
+
+
+def ToUnixMicros(t: Time) -> int:
+    return int(t) / 1e6
+
+
+def ToUnixSeconds(time: Time) -> int:
+    return int(t) / 1e9
+
+
+def ToDatetime(t: Time) -> datetime.datetime:
+    return datetime.datetime.fromtimestamp(int(t) / 1e9)
---
+++ xlab/base/time_test.py
@@ -0,0 +1,119 @@
+import datetime
+
+from absl.testing import absltest
+
+from xlab.base import time
+
+
+# Based on: https://github.com/abseil/abseil-cpp/blob/master/absl/time/time_test.cc
+class TimeTest(absltest.TestCase):
+
+    def test_const_expr(self):
+        self.assertEqual(time.Time(), time.UnixEpoch())
+        self.assertEqual(time.Time(), time.FromUnixNanos(0))
+        self.assertEqual(time.Time(), time.FromUnixMillis(0))
+        self.assertEqual(time.Time(), time.FromUnixMicros(0))
+        self.assertEqual(time.Time(), time.FromUnixSeconds(0))
+
+    def test_unix_epoch(self):
+        epoch = time.FromDatetime(datetime.datetime(1970, 1, 1, 0, 0, 0))
+        self.assertEqual(epoch, time.UnixEpoch())
+        self.assertEqual(epoch - time.UnixEpoch(), time.ZeroDuration())
+
+    def test_add(self):
+        d = time.Nanoseconds(1)
+        t0 = time.Time()
+        t1 = t0 + d
+        self.assertEqual(d, t1 - t0)
+        self.assertEqual(-d, t0 - t1)
+        self.assertEqual(t0, t1 - d)
+
+        t = t0
+        self.assertEqual(t0, t)
+        t += d
+        self.assertEqual(t0 + d, t)
+        self.assertEqual(d, t - t0)
+        t -= d
+        self.assertEqual(t0, t)
+
+        # Tests overflow between subseconds and seconds.
+        t = time.UnixEpoch()
+        t += time.Milliseconds(500)
+        self.assertEqual(time.UnixEpoch() + time.Milliseconds(500), t)
+        t += time.Milliseconds(600)
+        self.assertEqual(time.UnixEpoch() + time.Milliseconds(1100), t)
+        t -= time.Milliseconds(600)
+        self.assertEqual(time.UnixEpoch() + time.Milliseconds(500), t)
+        t -= time.Milliseconds(500)
+        self.assertEqual(time.UnixEpoch(), t)
+
+    def test_relational_operators(self):
+        t1 = time.Nanoseconds(0)
+        t2 = time.Nanoseconds(1)
+        t3 = time.Nanoseconds(2)
+        self.assertTrue(time.Time() == t1)
+        self.assertTrue(t1 == t1)
+        self.assertTrue(t2 == t2)
+        self.assertTrue(t3 == t3)
+
+        self.assertTrue(t1 < t2)
+        self.assertTrue(t2 < t3)
+        self.assertTrue(t1 < t3)
+
+        self.assertTrue(t1 <= t1)
+        self.assertTrue(t1 <= t2)
+        self.assertTrue(t2 <= t2)
+        self.assertTrue(t2 <= t3)
+        self.assertTrue(t3 <= t3)
+        self.assertTrue(t1 <= t3)
+
+        self.assertTrue(t2 > t1)
+        self.assertTrue(t3 > t2)
+        self.assertTrue(t3 > t1)
+
+        self.assertTrue(t2 >= t2)
+        self.assertTrue(t2 >= t1)
+        self.assertTrue(t3 >= t3)
+        self.assertTrue(t3 >= t2)
+        self.assertTrue(t1 >= t1)
+        self.assertTrue(t3 >= t1)
+
+    def test_floor_conversion(self):
+        self.assertEqual(
+            1, time.ToUnixNanos(time.UnixEpoch() + time.Nanoseconds(3) / 2))
+        self.assertEqual(
+            1, time.ToUnixNanos(time.UnixEpoch() + time.Nanoseconds(1)))
+        self.assertEqual(
+            0, time.ToUnixNanos(time.UnixEpoch() + time.Nanoseconds(1) / 2))
+        self.assertEqual(
+            0, time.ToUnixNanos(time.UnixEpoch() + time.Nanoseconds(0)))
+        # NOTE: the following three assertions are different from time.Time,
+        # as nanoseconds is the ultimate precison here.
+        self.assertEqual(
+            0, time.ToUnixNanos(time.UnixEpoch() - time.Nanoseconds(1) / 2))
+        self.assertEqual(
+            -1, time.ToUnixNanos(time.UnixEpoch() - time.Nanoseconds(1)))
+        self.assertEqual(
+            -1, time.ToUnixNanos(time.UnixEpoch() - time.Nanoseconds(3) / 2))
+
+        self.assertEqual(
+            -500, time.ToUnixNanos(time.UnixEpoch() - time.Microseconds(1) / 2))
+        self.assertEqual(
+            -1000, time.ToUnixNanos(time.UnixEpoch() - time.Microseconds(1)))
+        self.assertEqual(
+            -1500,
+            time.ToUnixNanos(time.UnixEpoch() - time.Microseconds(3) / 2))
+
+    def test_format_time(self):
+        t = time.FromDatetime(datetime.datetime(2015, 2, 3, 4, 5, 6))
+        self.assertEqual("2015-02-03T04:05:06+00:00", time.FormatTime(t))
+        t = time.FromDatetime(datetime.datetime(2015, 2, 3, 4, 5))
+        self.assertEqual("2015-02-03T04:05:00+00:00", time.FormatTime(t))
+        t = time.FromDatetime(datetime.datetime(2015, 2, 3, 4))
+        self.assertEqual("2015-02-03T04:00:00+00:00", time.FormatTime(t))
+        t = time.FromDatetime(datetime.datetime(2015, 2, 3))
+        self.assertEqual("2015-02-03T00:00:00+00:00", time.FormatTime(t))
+
+
+if __name__ == '__main__':
+    absltest.main()
