efbb6a76d31ec24b40660c94188aa1f8268f6159
dayfine <26423504+dayfine@users.noreply.github.com>
2020-06-09 00:04:38 -0700

    notebook runtime

--- xlab/colab/start_local_runtime.sh
+++ xlab/colab/start_local_runtime.sh
@@ -2,4 +2,6 @@
 bazel run //xlab/colab:start_local_runtime -- \
   --NotebookApp.allow_origin='https://colab.research.google.com' \
   --port=8888 \
-  --NotebookApp.port_retries=0
+  --NotebookApp.port_retries=0 \
+  # This shouldn't be necesary: https://github.com/dillon-giacoppo/rules_python_external/issues/18
+  --config=$(pwd)/.venv/etc/jupyter/jupyter_notebook_config.json
