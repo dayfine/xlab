fa6e4aed9f1313ccf48eaae26a1618ff35dd9e91
dayfine <26423504+dayfine@users.noreply.github.com>
2020-07-19 20:37:13 -1000

    make a map for different data types to be fetched

--- xlab/data/importer/iex/importer.py
+++ xlab/data/importer/iex/importer.py
@@ -7,9 +7,14 @@ from xlab.data.importer.iex import api
 from xlab.net.proto import time_util
 from xlab.util.status import errors
 
-_DATA_TYPE_MAP = {
+_DataType = data_type_pb2.DataType
 
+# A map of data types to their data fields on IEX Chart API's response.
+_DATA_TYPE_TO_CHART_FIELD_MAP = {
+    _DataType.CLOSE_PRICE: 'close',
+    _DataType.VOLUME: 'volume',
 }
+
 
 class IexDataImporter(importer.DataImporter):
 
@@ -35,25 +40,19 @@ class IexDataImporter(importer.DataImporter):
         chart_series = data[symbol]['chart']
 
         now = time.Now()
-        close_results = []
-        volume_results = []
+        data_types_to_import = _DATA_TYPE_TO_CHART_FIELD_MAP.keys()
+        results = {data_type: [] for data_type in data_types_to_import}
         for chart_data in chart_series:
             data_date = time.ParseCivilTime(chart_data['date'])
 
-            close_data = self._make_data_base(symbol, data_date, now)
-            close_data.data_type = data_type_pb2.DataType.CLOSE_PRICE
-            close_data.value = chart_data['close']
-            close_results.append(close_data)
+            for data_type in data_types_to_import:
+                data = self._make_data_base(symbol, data_date, now)
+                data.data_type = data_type
+                data.value = chart_data[
+                    _DATA_TYPE_TO_CHART_FIELD_MAP[data_type]]
+                results[data_type].append(data)
 
-            volume_data = self._make_data_base(symbol, data_date, now)
-            volume_data.data_type = data_type_pb2.DataType.VOLUME
-            volume_data.value = chart_data['volume']
-            volume_results.append(volume_data)
-
-        return {
-            data_type_pb2.DataType.CLOSE_PRICE: close_results,
-            data_type_pb2.DataType.VOLUME: volume_results
-        }
+        return results
 
     def _make_data_base(self, symbol: str, data_date: time.CivilTime,
                         now: time.time) -> data_entry_pb2.DataEntry:
